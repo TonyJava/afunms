8cc7fcdc34985001522ab009d1848b2e8599fb28
hanpei <happysoftware@foxmail.com>
2014-08-20 19:59:46 +0800

    侧边工具栏  鼠标移入和移出  样式变化，跨浏览器平台

--- WebContent/topology/network/js/toolbar.js
+++ WebContent/topology/network/js/toolbar.js
@@ -1,147 +1,234 @@
 // JavaScript Document
+/**
+ * 兼容ie和ff
+ * 当做时间函数调用，没有传入event时间对象，因此event通过arguments来获取
+ * @author HP
+ */
 function buttonNewOut() {
-	window.event.srcElement.className="button_new_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_new_out";
 }
 function buttonNewOver() {
-	window.event.srcElement.className="button_new_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_new_over";
 }
 
 function buttonEditMapOut() {
-	window.event.srcElement.className="button_editmap_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_editmap_out";
 }
 function buttonEditMapOver() {
-	window.event.srcElement.className="button_editmap_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_editmap_over";
 }
 function buttonSearchOut() {
-	window.event.srcElement.className="button_search_out";
+	
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_search_out";
 }
 function buttonSearchOver() {
-	window.event.srcElement.className="button_search_over";
+	
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_search_over";
 }
 function buttonShowTreeOut() {
-	window.event.srcElement.className="button_showtree_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_showtree_out";
 }
 function buttonShowTreeOver() {
-	window.event.srcElement.className="button_showtree_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_showtree_over";
 }
 function buttonOpenOut() {
-	window.event.srcElement.className="button_open_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_open_out";
 }
 function buttonOpenOver() {
-	window.event.srcElement.className="button_open_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_open_over";
 }
 
 
 function buttonEditOut() {
-	window.event.srcElement.className="button_edit_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_edit_out";
 }
 function buttonEditOver() {
-	window.event.srcElement.className="button_edit_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_edit_over";
 }
 
 
 function buttonSaveOut() {
-	window.event.srcElement.className="button_save_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_save_out";
 }
 function buttonSaveOver() {
-	window.event.srcElement.className="button_save_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_save_over";
 }
 
 function buttonSelectOut() {
-	window.event.srcElement.className="button_select_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_select_out";
 }
 function buttonSelectOver() {
-	window.event.srcElement.className="button_select_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_select_over";
 }
 
 
 function buttonRefreshOut() {
-	window.event.srcElement.className="button_refresh_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_refresh_out";
 }
 function buttonRefreshOver() {
-	window.event.srcElement.className="button_refresh_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_refresh_over";
 }
 
 
 function buttonFullscreenOut() {
-	window.event.srcElement.className="button_fullscreen_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_fullscreen_out";
 }
 function buttonFullscreenOver() {
-	window.event.srcElement.className="button_fullscreen_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_fullscreen_over";
 }
 
 
 function buttonViewOut() {
-	window.event.srcElement.className="button_view_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_view_out";
 }
 
 function buttonViewOver() {
-	window.event.srcElement.className="button_view_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_view_over";
 }
 
 function buttonCreate1Out() {
-	window.event.srcElement.className="button_create1_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create1_out";
 }
 
 function buttonCreate1Over() {
-	window.event.srcElement.className="button_create1_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create1_over";
 }
 
 function buttonCreate2Out() {
-	window.event.srcElement.className="button_create2_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create2_out";
 }
 
 function buttonCreate2Over() {
-	window.event.srcElement.className="button_create2_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create2_over";
 }
 
 function buttonCreate3Out() {
-	window.event.srcElement.className="button_create3_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create3_out";
 }
 
 function buttonCreate3Over() {
-	window.event.srcElement.className="button_create3_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create3_over";
 }
 
 function buttonCreate4Out() {
-	window.event.srcElement.className="button_create4_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create4_out";
 }
 
 function buttonCreate4Over() {
-	window.event.srcElement.className="button_create4_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create4_over";
 }
 function buttonCreate5Out() {
-	window.event.srcElement.className="button_create5_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create5_out";
 }
 
 function buttonCreate5Over() {
-	window.event.srcElement.className="button_create5_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create5_over";
 }
 function buttonCreate6Out() {
-	window.event.srcElement.className="button_create6_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create6_out";
 }
 
 function buttonCreate6Over() {
-	window.event.srcElement.className="button_create6_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create6_over";
 }
 function buttonCreate7Out() {
-	window.event.srcElement.className="button_create7_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create7_out";
 }
 
 function buttonCreate7Over() {
-	window.event.srcElement.className="button_create7_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_create7_over";
 }
 function buttonControllerOut() {
-	window.event.srcElement.className="button_controller_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_controller_out";
 }
 function buttonControllerOver() {
-	window.event.srcElement.className="button_controller_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_controller_over";
 }
 
 
 function buttonBrowseOut() {
-	window.event.srcElement.className="button_browse_out";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_browse_out";
 }
 function buttonBrowseOver() {
-	window.event.srcElement.className="button_browse_over";
+	var event = window.event || arguments.callee.caller.event;
+	var srcElement = event.srcElement || event.target;
+	srcElement.className="button_browse_over";
 }
